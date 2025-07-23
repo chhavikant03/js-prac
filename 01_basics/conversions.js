f0e1537525b1bf771f1794471fab90e70b5d6b52
chhavikant03 <chhavikant202@gmail.com>
2025-07-23 13:37:50 +0000

    conversions

---
+++ 01_basics/conversions.js
@@ -0,0 +1,11 @@
+let score = undefined;
+//console.log(typeof score);
+
+let valInNumber = Number(score);
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
