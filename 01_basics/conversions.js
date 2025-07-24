c13d8eddc49b15f0fe0e20605aebfa08bc5464a1
chhavikant03 <chhavikant202@gmail.com>
2025-07-24 07:29:01 +0000

    operations added

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -7,5 +7,24 @@ let valInNumber = Number(score);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// operationsss
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+ // string concatenation 
+
+ let str1 ="Hello"
+ let str2 = " Chhavi";
+//  let str3 = str1 +str2;
+//  console.log(str3);
+
+ console.log(1+2);
+ console.log("1" +2)
+ console.log("1"+"2")
+ console.log(1+"2")
+ console.log(1+2+"2")
+ console.log("1"+2+2)
