ae4e5aaffd9f8b260de8643ff0ca4aab6a1e4480
chhavikant03 <chhavikant202@gmail.com>
2025-07-24 15:06:01 +0000

    Strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,6 @@
+const name = "Chhavi"
+const repoCount = 8
+ // way to declare a string
+
+const gameName = new String('Chhavik')
+console.log(`Hello my name is ${name} and i have ${repoCount} repositories on my Github`)
