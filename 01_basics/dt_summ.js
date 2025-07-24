4144c70aa85da303c641d95fe52bcb26e2951ca3
chhavikant03 <chhavikant202@gmail.com>
2025-07-24 14:45:55 +0000

    data type summary

---
+++ 01_basics/dt_summ.js
@@ -0,0 +1,21 @@
+// primitive 
+// strings , numbers, boolean, null, undefined, symbol, bigInt
+//---------------------------------
+//1. Js is dynamically typed language 
+// 2. done at runtime 
+// 3. meaning we do not need to define the data type of the variable , the interpreter or the runtime environment dynamically checks the data type of the variable 
+//-------------------------------
+// reference (non primitive)
+//Arrays, objects, functions
+
+const score = 100;
+const anotherScore = 100.32;
+
+console.log(typeof anotherScore);
+
+//o/p => number
+
+const outsideTemp = null;
+console.log(typeof outsideTemp);
+// o/p => Obejct
+
