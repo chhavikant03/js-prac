d519cb28b4b827e227852a8e0fa1d7be80d53a46
chhavikant03 <chhavikant202@gmail.com>
2025-08-01 14:51:10 +0000

    more objectss

--- 02basics/obj2.js
+++ 02basics/obj2.js
@@ -23,6 +23,33 @@ const regularuser ={
 const obj1 ={ 1: "a", 2: "b"}
 const obj2 ={ 3: "c", 4: "d"}
 
-const obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2)
 // Object.assign(target, source) {}this is an empty object acting as a target
-console.log(obj3)
+
+const obj3={...obj1, ...obj2} //this one is spread method
+// console.log(obj3)
+
+// array ke andar bahut saare objects ha 
+const user = [
+    {
+        id: 1,
+        email: "c@gmail.com"
+    },
+    {
+        id: 1,
+        email: "c@gmail.com"
+    },
+    {
+        id: 1,
+        email: "c@gmail.com"
+    }
+]
+
+const res = console.log(user[1].email);
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
